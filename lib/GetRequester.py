b83d25e9de55ef743ed617b203ab657d33446bc1
Parker Parham <parkerparham@gmail.com>
2023-04-26 16:01:11 -0400

    Done with assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,10 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        response_body = response.content
+        return json.loads(response_body)
